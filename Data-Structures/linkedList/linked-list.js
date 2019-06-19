652d525af3db45981168a342930decb57589bd39
joseph <joseph.hangarter@yahoo.com>
2019-06-19 10:40:18 -0700

    modified linked list

---
+++ Data-Structures/linkedList/linked-list.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
+
+// Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
+
+class linkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  insert(value){
+    
+  }
+}
