a11bb83e343fedaf9613dfac4105454830b1e765
joseph <joseph.hangarter@yahoo.com>
2019-07-11 23:49:24 -0700

    method working

--- Data-Structures/linkedList/__tests__/linked-lists.test.js
+++ Data-Structures/linkedList/__tests__/linked-lists.test.js
@@ -15,13 +15,19 @@ Can properly return a collection of all the values that exist in the linked list
 
 describe('Linked Lists', () => {
   it('Can successfully instantiate an empty linked list', () => {
-    let node = new Node('foo');
-    expect(node).toBe(true);
+    // let node = new Node('foo');
+    // expect(node).toBe(true);
   })
 
   it('append (value) should append node to end of linked list', () => {
-    let node = new Node('bar')
     
+  })
+
+  it('return null if number is greater than length of list', () => {
+    // let node = linkedList.insert('foo');
+    // console.log(node)
+    // let number = linkedList.kthFromEnd(3)
+    // expect(number).toBe(null);
   })
 
 });
