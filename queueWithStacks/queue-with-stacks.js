0a261b5c4d2fac3263069b44dd2a666b94dcbae8
joseph <joseph.hangarter@yahoo.com>
2019-07-23 23:08:57 -0700

    did some work, but still need to fix enqueue method

--- queueWithStacks/queue-with-stacks.js
+++ queueWithStacks/queue-with-stacks.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const queueAndStacks = require('../stacksAndQueues/stacks-and-queues')
-
-
+const Node = require('../stacksAndQueues/node')
+const Stack = require('../stacksAndQueues/stack')
+const Queue = require('../stacksAndQueues/queue')
 
 class PseudoQueue {
   constructor() {
-    this.storage = new queueAndStacks.Stack();
-    this.shuffle = new queueAndStacks.Stack();
+    this.storage = new Stack;
+    this.shuffle = new Stack;
   }
 
   enqueue(value) {
-    console.log(this.storage)
     if(this.storage.peek() !== null) {
     let temp = null;
+    console.log('hi');
     while(this.storage.peek() !== null) {
       temp = this.storage.pop();
       this.shuffle.push(temp)
@@ -28,8 +28,15 @@ class PseudoQueue {
   }
 }
 
+let pseudoQueue = new PseudoQueue();
+let stack = new Stack();
+let queue = new Queue();
+
+pseudoQueue.enqueue('1');
+pseudoQueue.enqueue('2');
+pseudoQueue.enqueue('3');
+pseudoQueue.enqueue('4');
+
+console.log(pseudoQueue);
 
 module.exports = PseudoQueue;
-
-let pseudoQueue = new PseudoQueue();
-console.log(pseudoQueue.enqueue('orange'))
