e37e5d4f74405631a0819368f0c50d2c0e33e2f3
joseph <joseph.hangarter@yahoo.com>
2019-07-25 15:39:12 -0700

    back to arrays

--- stacksAndQueues/stack.js
+++ stacksAndQueues/stack.js
@@ -52,6 +52,11 @@ class Stack {
 
 let stack = new Stack()
 stack.push('yo')
+stack.push('1')
+stack.push('2')
+stack.push('3')
+stack.push('4')
+
 console.log(stack.pop())
 console.log(stack.pop())
 
