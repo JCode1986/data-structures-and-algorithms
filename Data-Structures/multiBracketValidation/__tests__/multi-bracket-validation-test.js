f2d7a44f10db867998d5d3efe03dc34c7a7efb4d
joseph <joseph.hangarter@yahoo.com>
2019-08-17 02:18:53 -0700

    just need tests

---
+++ Data-Structures/multiBracketValidation/__tests__/multi-bracket-validation-test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Stack = require('../../stacksAndQueues/stack');
+const Bracket = require('../multi-bracket-validation');
+
+console.log(multiBracketValidation());
+
+describe('Multi-bracket validation', () => {
+  it('return false if there is nothing in stack', () => {
+
+  });
+});
