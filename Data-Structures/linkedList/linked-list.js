6483bd82972ec3c5562769ea843284088460abf8
JCode1986 <joseph.hangarter@yahoo.com>
2019-08-18 11:17:53 -0700

    return added

--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -1,8 +1,5 @@
 'use strict';
 
-module.exports.Node;
-module.exports.linkedList;
-
 // Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
 
 // Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
@@ -14,21 +11,22 @@ class Node {
   }
 };
 
-class linkedList {
-  constructor(){
+class LinkedList {
+  constructor() {
     this.head = null;
     this.tail = null;
   }
   
   // takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance
-  insert(value){
+  insert(value) {
     let newNode = new Node(value)
     newNode.next = this.head;
     this.head = newNode;
+    return this.head;
   };
 
   // takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list
-  includes(value){
+  includes(value) {
     let current = this.head;
     while(current.next){
       current = current.next;
@@ -40,7 +38,7 @@ class linkedList {
   };
 
   // takes in no arguments and returns a string representing all the values in the Linked List
-  toString(){
+  toString() {
     let current = this.head;
     while(current !== null){
       current = current.next;
@@ -49,13 +47,14 @@ class linkedList {
     }
 };
 
+module.exports = Node; 
+module.exports = LinkedList;
 
-let list = new linkedList();
-console.log(list)
-list.insert('foo');
-list.insert('bar');
-list.insert('hello');
-console.log(list);
-console.log(list.includes('foo'));
-console.log(list.includes('fooooo'));
-console.log(list.toString())
+// let list = new LinkedList();
+// console.log(list)
+// list.insert('foo');
+// list.insert('bar');
+// list.insert('hello');
+// console.log(list);
+// console.log(list.includes('foo'));
+// console.log(list.includes('fooooo'));
