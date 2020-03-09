12312060da790c6882f064e40fce048a47e91d47
jc <joseph.hangarter@yahoo.com>
2020-03-08 19:48:18 -0700

    added more testing to linked lists

--- data-structures/linked-list/__tests__/linked-lists-test.js
+++ data-structures/linked-list/__tests__/linked-lists-test.js
@@ -5,15 +5,18 @@ const LinkedList = require('../linked-list');
 const list = new LinkedList();
 
 describe('Linked Lists', () => {
+
   it('Can successfully instantiate an empty linked list', () => {
     let actual = { head: null, tail: null };
     expect(list).toEqual(actual);
   })
+
   it('Can properly insert into the linked list', () => {
     let actual = list.insert('hi');
     let expected = list.head;
     expect(expected).toEqual(actual);
   })
+
   it('The head property will properly point to the first node in the linked list', () => {
     let next = list.insert('second');
     let head = list.insert('first');
@@ -21,10 +24,49 @@ describe('Linked Lists', () => {
     let actual = [head, next]
     expect(expected).toEqual(actual);
   })
-  // it('Returns boolean if node\'s value exists in the linked list', () => {
-  //   list.insert('hello world')
-  //   let expected = true
-  //   let actual = list.includes('hello world')
-  //   expect(expected).toBe(actual);    
+
+  it('Returns boolean if node\'s value exists in the linked list', () => {
+    let expected = [true, false]
+    let actual = [list.includes('first'), list.includes('this will return false')]
+    expect(expected).toEqual(actual);    
+  })
+
+  it('Returns all node\'s values in the linked list', () => {
+    let expected = 'Head:[first] --> [second] --> [hi] --> null'
+    let actual = list.toString()
+    expect(expected).toBe(actual);    
+  })
+
+  it('Can append a node to an empty linked list', () => {
+    let empty = new LinkedList()
+    let actual = empty.append(1986)
+    let expected = empty.head
+    expect(expected).toBe(actual);    
+  })
+
+  it('Can append a node to the end of a linked list', () => {
+    let actual = list.append('Luke')
+    let expected = list.tail  
+    expect(expected).toBe(actual); 
+  })
+
+  // it('Return error message if node does not exist for insert before method', () => {
+  //   let actual = list.insertBefore('hi', 23)
+  //   let expected = []
+  //   console.log(list.toString())   
+  //   expect(expected).toBe(actual); 
   // })
+  
+  it('Can insert before an existing specified node in the linked list', () => {
+    let actual = list.insertBefore('hi', 23)
+    let expected = 'Head:[first] --> [second] --> [23] --> [hi] --> [Luke] --> null'
+    expect(expected).toBe(actual); 
+  })
+
+  it('Can insert after an existing specified node in the linked list', () => {
+    let actual = list.insertAfter('hi', 'world')
+    let expected = 'Head:[first] --> [second] --> [23] --> [hi] --> [world] --> [Luke] --> null'
+    expect(expected).toBe(actual); 
+  })
+
 });
--- data-structures/linked-list/linked-list.js
+++ data-structures/linked-list/linked-list.js
@@ -5,10 +5,10 @@
 // Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
 
 class Node {
-  constructor(value, next){
+  constructor(value, next) {
     this.value = value;
     this.next = next;
-  }
+  };
 };
 
 class LinkedList {
@@ -19,131 +19,114 @@ class LinkedList {
   
   // takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance
   insert(value) {
-    this.head = new Node(value, this.head)
-    return this.head
+    this.head = new Node(value, this.head);
+    return this.head;
   };
 
   // takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list
   includes(value) {
     let current = this.head;
-    while(current.next){
-      current = current.next;
+    while(current) {
       if(current.value == value){
         return true;
-      }
-    }
+      };
+      current = current.next;
+    };
     return false;
   };
 
   // takes in no arguments and returns a string representing all the values in the Linked List
   toString() {
     let current = this.head;
-    while(current !== null){
-      current = current.next;
-      return current.value.toString();
-      }
-    }
-
-    //which adds a new node with the given value to the end of the list
-    append(value) {
-      if(!this.head) {
-        this.head = new Node(value)
-        return;
-      }
-      let current = this.head;
-      while(current.next){
-        current = current.next;
-      }
-      current.next = new Node(value)
-      this.tail = current.next;
-    };
-
-    //add a new node with the given newValue immediately before the first value node
-    insertBefore(value, newVal) {
-      let node = new Node(newVal);
-      let current = this.head;
-      while(current.next){
-        if(current.next.value == value) {
-          node.next = current.next; 
-          current = node;
-        }
-        current = current.next;
-      } 
-    };
-
-    //add a new node with the given newValue immediately after the first value node
-    insertAfter(value, newVal) {
-      let node = new Node(newVal);
-      let current = this.head;
-      while(current.next){
-        if(current.value == value) {
-          node = current.next; 
-          current = node;
-        }
-        current = current.next;
-      } 
-    };
-
-
-    kthFromEnd(k) { 
-      let count =  0;
-      let current = this.head;
-
-    while(current.next !== null) {
+    let str = `Head:`;
+    while(current){
+      str += `[${current.value}] --> `;
       current = current.next
-      count++;
-    } 
-    if (k > count) {
-      return null;
-    }
-    let target = count - k;
-    current = this.head;
-    while(target > 0) {
+      };
+      str += 'null';
+      return str;
+    };
+
+    getSize() {
+      return this.size
+    };
+
+  //which adds a new node with the given value to the end of the list
+  append(value) {
+    if(!this.head) {
+      this.size++;
+      return this.insert(value);
+    };
+    let node = new Node(value);
+    let current = this.head;
+    while(current) {
+      if(current.next == null) {
+        current.next = node;
+        node.next = null;
+        this.tail = node;
+      };
+      this.size++;
       current = current.next;
-      target--;
-    }
-    return current.value
+    };
+    return this.tail;
+  };
+
+  //add a new node with the given newValue immediately before the first value node
+  insertBefore(value, newVal) {
+    let node = new Node(newVal);
+    let current = this.head;
+    let temp = null
+    while(current) {
+      if(current.value == value) {
+        temp.next = node
+        node.next = current
+      } 
+      temp = current
+      current = current.next;
+    }; 
+    return this.toString();
+  };
+
+  //add a new node with the given newValue immediately after the first value node
+  insertAfter(value, newVal) {
+    let node = new Node(newVal);
+    let current = this.head;
+    let temp = null
+    while(current){
+      if(current.value == value) {
+        temp = current.next
+        current.next = node
+        node.next = temp
+      };
+      current = current.next;
+    };
+    return this.toString();
+  };
+
+
+  kthFromEnd(k) { 
+    let count =  0;
+    let current = this.head;
+
+  while(current.next !== null) {
+    current = current.next
+    count++;
+  } 
+  if (k > count) {
+    return null;
+  }
+  let target = count - k;
+  current = this.head;
+  while(target > 0) {
+    current = current.next;
+    target--;
+  }
+  return current.value
   };
 
   //merge link lists together, alternating nodes starting with param 1 (list1)
   mergeLists(list1, list2) {
-    // let zippedList = new LinkedList();
-    // let currentOne = list1.head;
-    // let currentTwo = list2.head;
-    // let countOne = 0;
-    // let countTwo = 0;
-    // while (currentOne !== null) {
-    //   countOne++;
-    //   currentOne = currentOne.next;
-    // }
-    // while (currentTwo !== null) {
-    //   countTwo++;
-    //   currentTwo = currentTwo.next;
-    // }
-    // currentOne = list1.head;
-    // currentTwo = list2.head;
-    // if (countOne >= countTwo) {
-    //   while (currentOne !== null) {
-    //     let nextOne = currentOne.next;
-    //     let nextTwo = currentTwo.next;
-    //     currentOne.next = currentTwo.value;
-    //     zippedList.append(currentOne);
-    //     currentTwo.next = nextOne;
-    //     zippedList.append(currentTwo);
-    //     currentOne = nextOne;
-    //     currentTwo = nextTwo;
-    //   }
-    // } else{
-    //   while (currentOne !== null || currentTwo !== null){
-    //     let nextOne = currentOne.next;
-    //     let nextTwo = currentTwo.next;
-    //     currentTwo.next = currentOne.value;
-    //     zippedList.append(currentTwo);
-    //     currentOne.next = nextTwo;
-    //     zippedList.append(currentOne);
-    //     currentOne = nextOne;
-    //     currentTwo = nextTwo;
-    //   }}
+
   } 
 };
 
