5c9d41e522d312300c15936bbeb282ca0e866589
Joseph Hangarter <joseph.hangarter@yahoo.com>
2021-08-03 20:02:26 -0700

    added selection sort solution

--- sorts/selectionSort/selection-sort.js
+++ sorts/selectionSort/selection-sort.js
@@ -3,11 +3,17 @@
 let array = [8, 7, 6, 5, 4, 3, 2, 1];
 
 function selectionSort(arr) {
-    let temp = 0;
-    let min = arr[0];
-    for(let i = 0; i < arr.length - 1; i++) {
-        if(arr[i + 1] < min) {
-            
+    let temp;
+    let min;
+    for(let i = 0; i < arr.length; i++) {
+        min = arr[i];
+        for(let j = i + 1; j < arr.length; j++) {
+            if(min > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp; 
+            }
         }
     }
+    return arr;
 }
