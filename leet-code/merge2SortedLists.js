bd32ae4112d2392710e46b38a8995a341d1a3e3a
JC <joseph.hangarter@yahoo.com>
2021-09-17 23:59:42 -0700

    added merge 2 sorted lists

---
+++ leet-code/merge2SortedLists.js
@@ -0,0 +1,12 @@
+var mergeTwoLists = function(l1, l2) {
+    let result = null;
+
+    if(l1 == null) return l2; 
+    if(l2 == null) return l1;
+    
+    result = (l1.val <= l2.val) ? l1 : l2;
+    result.next = (l1.val <= l2.val) ?  mergeTwoLists(l1.next, l2) : mergeTwoLists(l1, l2.next);
+
+    return result;
+    
+};
