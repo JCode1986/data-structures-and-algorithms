f6d49e02449013bf7fb9bcfbe169b2c330342509
JC <joseph.hangarter@yahoo.com>
2021-09-24 23:37:53 -0700

    added solution for best time to buy and sell stocks

---
+++ leet-code/bestTimeToBuyAndSell.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Examples:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+var maxProfit = function(prices) {
+    let lowestPrice = prices[0];
+    let diff = 0;
+    for(let i=1; prices.length > i; i++){
+        lowestPrice = Math.min(lowestPrice, prices[i]);
+        diff = Math.max(diff, prices[i] - lowestPrice);
+    }
+return diff;   
+};
